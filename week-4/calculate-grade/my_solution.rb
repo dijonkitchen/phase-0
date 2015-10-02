28c470927483700e3d26d24014eee5a01c9d46d5
dijonkitchen <jschen2008@gmail.com>
2015-10-01 21:57:44 -0400

    Add remaining assignments

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,19 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def get_grade(grade)
+  if grade >= 90
+    "A"
+  elsif grade >= 80
+    "B"
+  elsif grade >= 70
+    "C"
+  elsif grade >= 60
+    "D"
+  else
+    "F"
+  end
+end
