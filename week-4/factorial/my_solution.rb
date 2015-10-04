db7ce0e30b69eea229dd26ccc5f163950da5d280
dijonkitchen <jschen2008@gmail.com>
2015-10-04 14:41:55 -0400

    Solve 4.7.1

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,36 @@
+# Factorial
+
+# I worked on this challenge with: Jon Chen and Armani Saldana.
+
+
+# Your Solution Below
+def factorial(number)
+  # Your code goes here
+
+  total = 1
+
+  for i in number.downto(1) do
+    total *= i
+  end
+
+  # # Store number as count
+  # count = number
+  #
+  # # Create an empty array
+  # all_numbers = []
+  #
+  # # Loop until zero
+  # while count > 0 do
+  #   # Store number in array
+  #   all_numbers << count
+  #   # Decrease count by one
+  #   count -= 1
+  # end
+  #
+  # total = 1
+  #
+  # all_numbers.each {|n| total *= n}
+  #
+  return total
+
+end
