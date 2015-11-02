2d86b4c7cc207f4f796ecb05db679f6a623b3a89
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2015-11-02 12:15:41 -0500

    Add duplicate reflection

--- week-8/oo-basics.rb
+++ week-8/oo-basics.rb
@@ -246,4 +246,13 @@ p binary_search(students, "NOT A STUDENT", 0, students.size - 1) == -1
 
 # Reflection
 
-# In ruby_review.rb file
+# As replicated in ruby_review.rb file
+
+=begin
+What concepts did you review or learn in this challenge?
+I reviewed some uses of class methods and variables. I learned that binary search was trickier than I thought. Originally I wanted to use the size of the array instead of inputing the low and high index values, but since I need to return the original array's index, I need to keep the whole index structure to return it.
+What is still confusing to you about Ruby?
+I was able to find a bsearch method for Ruby but wasn't sure how to use it properly for this task.
+What are you going to study to get more prepared for Phase 1?
+Algorithms and data structures are really interesting, so I might look into that depending on next week's work load. 
+=end
